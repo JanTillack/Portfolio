b8024ac306b22fc3b1cb08914bf8c0a125a4dce8
Jan Tillack <ford_gt90@wp.pl>
2016-08-20 09:48:18 +0200

    Gotta do all of the components

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,14 @@
 module.exports = function(grunt) {
 
 	grunt.initConfig({
-
+		less: {
+			dev: {
+				files: {
+					'looks.css':'style/assets.less'
+				}
+			}
+		}
 	});
 	grunt.loadNpmTasks('grunt-serve');
+	grunt.loadNpmTasks('grunt-contrib-less');
 };
